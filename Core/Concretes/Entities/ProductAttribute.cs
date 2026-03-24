11d11a5c6e4fb6ece12edad7622cfd3e78383d06
ahmetcanayhan <ahmetayhan38@icloud.com>
2026-03-24 14:50:09 +0300

    Add project files.

---
+++ Core/Concretes/Entities/ProductAttribute.cs
@@ -0,0 +1,16 @@
+﻿namespace Core.Concretes.Entities
+{
+    public class ProductAttribute
+    {
+        public int Id { get; set; }
+        public string Key { get; set; } = null!;
+        public string Value { get; set; } = null!;
+
+        // Foreign Keys
+        public int ProductId { get; set; }
+
+        // Navigation Properties
+
+        public virtual Product? Product { get; set; }
+    }
+}
---
+++ Core/Concretes/Entities/Subcategory.cs
@@ -0,0 +1,17 @@
+﻿using Core.Abstracts.Bases;
+
+namespace Core.Concretes.Entities
+{
+    public class Subcategory : BaseEntity
+    {
+        public string Name { get; set; } = null!;
+
+        // Foreign Keys
+        public int CategoryId { get; set; }
+
+        // Navigation Properties
+        public virtual Category? Category { get; set; }
+        public virtual ICollection<Product> Products { get; set; } = [];
+
+    }
+}
