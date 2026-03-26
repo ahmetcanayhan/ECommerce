d403e1ba9968bb862fae32a8239676e03e6bcd95
ahmetcanayhan <ahmetayhan38@icloud.com>
2026-03-26 12:19:55 +0300

    Update 01 26.03.2026

---
+++ Business/IOC.cs
@@ -0,0 +1,35 @@
+﻿using Business.Profiles;
+using Business.Services;
+using Core.Abstracts;
+using Core.Abstracts.IServices;
+using Core.Concretes.Entities;
+using Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Business
+{
+    public static class IOC
+    {
+        public static IServiceCollection AddCustomServices (this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ShopContext>(opt => opt.UseSqlite(configuration.GetConnectionString("shop_db")));
+
+            services.AddIdentity<Customer, IdentityRole>()
+                    .AddEntityFrameworkStores<ShopContext>()
+                    .AddDefaultTokenProviders();
+
+            services.AddAutoMapper(cfg =>
+            {
+                cfg.AddProfile<ShowroomProfiles>();
+            });
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<IShowroomService, ShowroomService>();
+
+            return services;
+        } 
+    }
+}
---
+++ Business/Services/ShowroomService.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using Core.Abstracts;
+using Core.Abstracts.IServices;
+using Core.Concretes.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utils.Responses;
+
+namespace Business.Services
+{
+    public class ShowroomService : IShowroomService
+    {
+        private readonly IUnitOfWork unitOfWork;
+        private readonly IMapper mapper;
+        public ShowroomService(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            this.unitOfWork = unitOfWork;
+            this.mapper = mapper;
+        }
+
+        public async Task<IResult<IEnumerable<ProductListItemDto>>> GetProductsAsync()
+        {
+            var result = await unitOfWork.ProductRepository.FindManyAsync(x => x.Active && !x.Deleted, "Subcategory.Category", "Brand", "Images", "Reviews");
+            if (result.IsSuccess)
+            {
+                var products = mapper.Map<IEnumerable<ProductListItemDto>>(result.Data);
+                return Result<IEnumerable<ProductListItemDto>>.Success(products);
+            }
+            return Result<IEnumerable<ProductListItemDto>>.Failure(result.Errors, result.StatusCode);
+        }
+    }
+}
